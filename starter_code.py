16f8b3cf49d065da97526721b4efcac8c4d9b016
SeunnnA <117874856+SeunnnA@users.noreply.github.com>
2023-10-31 12:34:31 -0400

    Create starter_code.py

---
+++ starter_code.py
@@ -0,0 +1,64 @@
+# Import the required libraries for mathematical and random operations
+import math
+import random
+
+# Function to calculate the horizontal distance a projectile can travel
+def calculate_projectile_distance(launch_angle, initial_velocity):
+    # Convert the launch angle from degrees to radians
+    # Calculate the horizontal distance traveled by the projectile and return it
+    # Use the projectile motion formula: d = (v^2 * sin(2 * theta)) / g
+   
+
+# Function to play the Raging Rabbits game
+def raging_rabbits():
+    # Display a welcome message to the player
+    # Provide instructions for the game
+    # Ask the player to input the initial launch velocity for the cannon
+    # Store the input in a variable
+    
+
+    # Define the minimum and maximum launch angles for the target range
+    # min_angle = 10 degrees, max_angle = 45 degrees
+    
+    # Calculate the minimum and maximum distances that the cannon can hit
+    # Use the calculate_projectile_distance function with min_angle and max_angle
+    
+    # Display the range of distances the player can hit
+    # Show the calculated minimum and maximum distances
+
+    # Generate a random target distance within the range
+    # Use the random library to choose a random integer between min_distance and max_distance
+
+    # Display the randomly selected target distance
+    # Show the chosen target distance
+
+    # Allow the player to make up to 5 attempts to hit the target
+    # Use a loop that iterates 5 times for each attempt
+
+        # Display the current attempt number
+        # Show the attempt number to the player
+
+        # Ask the player to enter the launch angle in degrees
+        # Store the input in a variable
+
+        # Calculate the distance traveled by the projectile
+        # Use the calculate_projectile_distance function with the entered launch angle
+
+        # Display the calculated distance
+        # Show the distance the projectile traveled
+
+        # Calculate the error, which is the difference between the target distance and the actual distance
+        # Calculate the absolute error (positive value) for comparison
+
+        # Check if the player hit the target within a 5-meter range
+        # If the absolute error is less than or equal to 5, the player hit the target
+
+            # Display a success message if the player hit the target
+            # Show a success message to the player
+
+    # Display a message if the player was not able to hit the target in 5 attempts
+    # Show a message to the player indicating they didn't hit the target
+
+# Call the Raging Rabbits game function if the script is executed directly
+if __name__ == '__main__':
+    # Execute the Raging Rabbits game function
